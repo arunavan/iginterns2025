81d2e2fe3897f06e3a487c6505b9e197105432bd
Vangala Aruna <arunajava567@gmail.com>
2025-02-13 11:11:06 +0530

    Add files via upload

--- day5/exceptions/ExDemo3.java
+++ day5/exceptions/ExDemo3.java
@@ -3,12 +3,21 @@ package com.insight.day5.exceptions;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 
-
-public class ExDemo3 {
-
-	public  void disp() throws  InterruptedException,FileNotFoundException {
+class X{
+	public void disp() throws  InterruptedException,FileNotFoundException{
 		Thread.sleep(500);//checked
 		FileReader f=new FileReader("c:\\abc.txt");//checked, compiletime
+		
+	}
+}
+public class ExDemo3 extends X {
+    @Override
+	public  void disp() throws  InterruptedException ,FileNotFoundException 
+    {
+		Thread.sleep(500);//checked
+		FileReader f=new FileReader("c:\\abc.txt");//checked, compiletime
+		int x[]= {5,6,7};
+		System.out.println(x[7]);
 				
 	}
 	public static void main(String[] args) throws  InterruptedException,FileNotFoundException{
